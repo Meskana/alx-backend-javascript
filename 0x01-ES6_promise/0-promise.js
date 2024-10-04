39933cb9bfb179e58b6ce7f198429e732d079be4
marcelinus <marcelinusilonuba21@gmail.com>
2024-10-04 14:37:09 +0100

    ok

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
-export = function getResponseFromAPI() {
+function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     const sucess = true;
     if (sucess) {
@@ -7,4 +7,4 @@ export = function getResponseFromAPI() {
       reject("404!");
     }
   });
-};
+}
