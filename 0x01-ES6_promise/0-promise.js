5f5aa45de004811358505557079522a02bc636d0
marcelinus <marcelinusilonuba21@gmail.com>
2024-10-04 13:06:39 +0100

    task1

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const sucess = true;
+    if (sucess) {
+      resolve(sucess);
+    } else {
+      reject("404!");
+    }
+  });
+}
